25cbbd3c605d3667e306be404e693db6d256cb85
Felix Dobler <felix.dobler.fd@gmail.com>
2023-06-03 21:54:01 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Setup
+
+- Use `.env.example` as a reference to create your own `.env` file and adjust the values
+- Add a cronjob that executes `main.py` once a month.
+- The log is appended to `netflixReminder.log` in relative location to `main.py`
---
+++ main.py
@@ -0,0 +1,34 @@
+#!/bin/python
+import asyncio
+import os
+from dotenv import load_dotenv
+import telegram
+import logging
+
+logging.basicConfig(
+    filename='netflixReminder.log',
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+logger = logging.getLogger(__name__)
+
+
+AMOUNT = 3.6
+REMINDER_MSG = f"Bitte bezahlen:\nhttps://www.paypal.com/paypalme/FelixDobler/{AMOUNT}"
+
+async def main():
+    bot = telegram.Bot(os.getenv('TELEGRAM_API_TOKEN'))
+    async with bot:
+
+        logger.info('Sending message: %s', REMINDER_MSG)
+        try:
+            # await asyncio.sleep(10)
+            await bot.send_message(text=REMINDER_MSG, chat_id=os.getenv('DEBUG_USER_ID'))
+            logger.info('Sent reminder')
+        except Exception as ex:
+            logger.error('Sending reminder failed: %s', ex)
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    asyncio.run(main())
